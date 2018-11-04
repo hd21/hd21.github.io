ccb43f9754113ccf04b920edf88ea14c92452fc4
Hazel Doctor <hazel@dentalpost.net>
2018-11-04 01:34:44 -0400

    Add about page and image

--- js/script.js
+++ js/script.js
@@ -81,3 +81,35 @@ $('#return-to-top').click(function() {
     600
   );
 });
+
+$(window).scroll(function() {
+  $('.highlight').each(function() {
+    if ($(this).onHighlight()) {
+      $(this).addClass('show');
+    } else {
+      $(this).removeClass('show');
+    }
+  });
+});
+
+$.prototype.onHighlight = function() {
+  var window = $(window);
+
+  var viewport = {
+    top: window.scrollTop(),
+    left: window.scrollLeft(),
+  };
+  viewport.right = viewport.left + window.width();
+  viewport.bottom = viewport.top + window.height();
+
+  var boundary = this.offset();
+  boundary.right = boundary.left + this.outerWidth();
+  boundary.bottom = boundary.top + this.outerHeight();
+
+  return !(
+    viewport.right < boundary.left ||
+    viewport.left > boundary.right ||
+    viewport.bottom < boundary.bottom ||
+    viewport.top > boundary.bottom
+  );
+};
